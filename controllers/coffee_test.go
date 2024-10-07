9791ee8657b2b5142a88d8282cdee49f882ffd6d
Daniel <danielboone1990@gmail.com>
2024-10-07 12:46:24 +0700

    Add tests

--- controllers/coffee_test.go
+++ controllers/coffee_test.go
@@ -1,13 +1,16 @@
 package controllers_test
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"net/http"
 	"net/http/httptest"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/stretchr/testify/mock"
 
 	"coffee/coffee-server/controllers"
 	"coffee/coffee-server/mocks"
@@ -15,18 +18,16 @@ import (
 )
 
 var (
-	mock     *mocks.CoffeeService
-	recorder *httptest.ResponseRecorder
-	request  *http.Request
+	mockedCoffee *mocks.CoffeeService
+	recorder     *httptest.ResponseRecorder
+	request      *http.Request
 )
-
-var ()
 
 var _ = Describe("Coffee controller", func() {
 	BeforeEach(func() {
 		recorder = httptest.NewRecorder()
 
-		mock = new(mocks.CoffeeService)
+		mockedCoffee = new(mocks.CoffeeService)
 	})
 
 	Describe("GetAllCoffees", func() {
@@ -39,9 +40,9 @@ var _ = Describe("Coffee controller", func() {
 				{Name: "Espresso", Roast: "Dark", Image: "image3.png", Region: "Italy", Price: 10.0, GrindUnit: 2},
 				{Name: "Cappuccino", Roast: "Medium", Image: "image4.png", Region: "Italy", Price: 11.0, GrindUnit: 1},
 			}
-			mock.On("GetAllCoffees").Return(mockCoffees, nil)
+			mockedCoffee.On("GetAllCoffees").Return(mockCoffees, nil)
 
-			controllers.GetAllCoffees(recorder, request, mock)
+			controllers.GetAllCoffees(recorder, request, mockedCoffee)
 
 			Expect(recorder.Code).To(Equal(http.StatusOK))
 
@@ -55,8 +56,8 @@ var _ = Describe("Coffee controller", func() {
 		})
 
 		It("should log the error and not write a response", func() {
-			mock.On("GetAllCoffees").Return(nil, errors.New("New database error"))
-			controllers.GetAllCoffees(recorder, request, mock)
+			mockedCoffee.On("GetAllCoffees").Return(nil, errors.New("New database error"))
+			controllers.GetAllCoffees(recorder, request, mockedCoffee)
 
 			Expect(recorder.Code).To(Equal(http.StatusInternalServerError))
 
@@ -79,7 +80,7 @@ var _ = Describe("Coffee controller", func() {
 		})
 		It("Return coffee by id", func() {
 
-			mockCoffees := &services.Coffee{ // Use & to create a pointer to Coffee
+			mockCoffee := &services.Coffee{ // Use & to create a pointer to Coffee
 				Name:      "Latte",
 				Roast:     "Light",
 				Image:     "image2.png",
@@ -87,9 +88,9 @@ var _ = Describe("Coffee controller", func() {
 				Price:     12.0,
 				GrindUnit: 1,
 			}
-			mock.On("GetCoffeesById", "").Return(mockCoffees, nil)
+			mockedCoffee.On("GetCoffeesById", "").Return(mockCoffee, nil)
 
-			controllers.GetCoffeesById(recorder, request, mock)
+			controllers.GetCoffeesById(recorder, request, mockedCoffee)
 
 			Expect(recorder.Code).To(Equal(http.StatusOK))
 
@@ -102,9 +103,9 @@ var _ = Describe("Coffee controller", func() {
 			Expect(response["coffee"].Price).To(Equal(float32(12.0)))
 		})
 		It("Return error if not coffee not found", func() {
-			mock.On("GetCoffeesById", "").Return(nil, errors.New("The coffee is not found"))
+			mockedCoffee.On("GetCoffeesById", "").Return(nil, errors.New("The coffee is not found"))
 
-			controllers.GetCoffeesById(recorder, request, mock)
+			controllers.GetCoffeesById(recorder, request, mockedCoffee)
 
 			Expect(recorder.Code).To(Equal(http.StatusInternalServerError))
 
@@ -121,7 +122,205 @@ var _ = Describe("Coffee controller", func() {
 			Expect(response["message"]).To(Equal("The coffee is not found"))
 		})
 	})
-	// Describe("CreateCoffee", func() {})
-	// Describe("UpdateCoffee", func() {})
-	// Describe("DeleteCoffe", func() {})
+
+	Describe("CreateCoffee", func() {
+		var (
+			mockCoffeeJson []byte
+			mockCoffee     services.Coffee
+		)
+
+		It("Create coffee should be succesfull", func() {
+			mockCoffee = services.Coffee{
+				ID:        "",
+				Name:      "Latte",
+				Roast:     "Light",
+				Image:     "image2.png",
+				Region:    "Colombia",
+				Price:     12.0,
+				GrindUnit: 1,
+				CreatedAt: time.Now(),
+				UpdatedAt: time.Now(),
+			}
+			// Marshal the struct to JSON
+			mockCoffeeJson, _ = json.Marshal(mockCoffee)
+			request, _ = http.NewRequest(http.MethodPost, "/api/v1/coffees/coffee", bytes.NewBuffer(mockCoffeeJson))
+			request.Header.Set("Content-Type", "application/json")
+			mockedCoffee.On("CreateCoffee", mock.MatchedBy(func(c services.Coffee) bool {
+				return c.Name == mockCoffee.Name &&
+					c.Roast == mockCoffee.Roast &&
+					c.Image == mockCoffee.Image &&
+					c.Region == mockCoffee.Region &&
+					c.Price == mockCoffee.Price &&
+					c.GrindUnit == mockCoffee.GrindUnit
+			})).Return(&mockCoffee, nil)
+
+			controllers.CreateCoffee(recorder, request, mockedCoffee)
+			Expect(recorder.Code).To(Equal(http.StatusOK))
+
+			mockedCoffee.AssertCalled(GinkgoT(), "CreateCoffee", mock.MatchedBy(func(c services.Coffee) bool {
+				return c.Name == mockCoffee.Name &&
+					c.Roast == mockCoffee.Roast &&
+					c.Image == mockCoffee.Image &&
+					c.Region == mockCoffee.Region &&
+					c.Price == mockCoffee.Price &&
+					c.GrindUnit == mockCoffee.GrindUnit
+			}))
+		})
+		It("Create coffee should be fails return error", func() {
+			mockCoffee = services.Coffee{ // Use & to create a pointer to Coffee
+				ID:        "",
+				Name:      "Latte",
+				Roast:     "Light",
+				Image:     "image2.png",
+				Region:    "Colombia",
+				Price:     12.0,
+				GrindUnit: 1,
+				CreatedAt: time.Now(),
+				UpdatedAt: time.Now(),
+			}
+			// Marshal the struct to JSON
+			mockCoffeeJson, _ = json.Marshal(mockCoffee)
+			request, _ = http.NewRequest(http.MethodPost, "/api/v1/coffees/coffee", bytes.NewBuffer(mockCoffeeJson))
+			request.Header.Set("Content-Type", "application/json")
+			mockedCoffee.On("CreateCoffee", mock.MatchedBy(func(c services.Coffee) bool {
+				return c.Name == mockCoffee.Name &&
+					c.Roast == mockCoffee.Roast &&
+					c.Image == mockCoffee.Image &&
+					c.Region == mockCoffee.Region &&
+					c.Price == mockCoffee.Price &&
+					c.GrindUnit == mockCoffee.GrindUnit
+			})).Return(nil, errors.New("Database error"))
+
+			controllers.CreateCoffee(recorder, request, mockedCoffee)
+			Expect(recorder.Code).To(Equal(http.StatusInternalServerError))
+
+			responseBody := recorder.Body.String()
+			Expect(responseBody).To(ContainSubstring("Database error"))
+		})
+		It("should return an error when the JSON decoder fails", func() {
+			invalidJson := `{"Name": "Latte", "Roast": "Light", "Price": "invalid_number"}`
+
+			request, _ := http.NewRequest(http.MethodPost, "/api/v1/coffees/coffee", bytes.NewBuffer([]byte(invalidJson)))
+			request.Header.Set("Content-Type", "application/json")
+
+			// Call the handler function with the invalid request
+			controllers.CreateCoffee(recorder, request, mockedCoffee)
+
+			// Assert that the status code is 500 Internal Server Error
+			Expect(recorder.Code).To(Equal(http.StatusInternalServerError))
+
+			responseBody := recorder.Body.String()
+			Expect(responseBody).To(ContainSubstring("cannot unmarshal string into Go struct field Coffee.price of type float32"))
+		})
+	})
+	Describe("UpdateCoffee", func() {
+		var (
+			mockCoffeeJson []byte
+			mockCoffee     services.Coffee
+		)
+		It("Should successfully update a coffee", func() {
+			mockCoffee = services.Coffee{
+				ID:        "",
+				Name:      "Latte",
+				Roast:     "Light",
+				Image:     "image2.png",
+				Region:    "Colombia",
+				Price:     12.0,
+				GrindUnit: 1,
+				CreatedAt: time.Now(),
+				UpdatedAt: time.Now(),
+			}
+			// Marshal the struct to JSON
+			mockCoffeeJson, _ = json.Marshal(mockCoffee)
+			request, _ = http.NewRequest(http.MethodPut, "/api/v1/coffees/coffee/12345", bytes.NewBuffer(mockCoffeeJson))
+			request.Header.Set("Content-Type", "application/json")
+			mockedCoffee.On("UpdateCoffee", "", mock.MatchedBy(func(c services.Coffee) bool {
+				return c.Name == mockCoffee.Name &&
+					c.Roast == mockCoffee.Roast &&
+					c.Image == mockCoffee.Image &&
+					c.Region == mockCoffee.Region &&
+					c.Price == mockCoffee.Price &&
+					c.GrindUnit == mockCoffee.GrindUnit
+			})).Return(&mockCoffee, nil)
+
+			controllers.UpdateCoffeeById(recorder, request, mockedCoffee)
+
+			Expect(recorder.Code).To(Equal(http.StatusOK))
+
+			mockedCoffee.AssertCalled(GinkgoT(), "UpdateCoffee", "", mock.MatchedBy(func(c services.Coffee) bool {
+				return c.Name == mockCoffee.Name &&
+					c.Roast == mockCoffee.Roast &&
+					c.Image == mockCoffee.Image &&
+					c.Region == mockCoffee.Region &&
+					c.Price == mockCoffee.Price &&
+					c.GrindUnit == mockCoffee.GrindUnit
+			}))
+		})
+
+		It("Should fail when coffee is invalid", func() {
+			invalidJson := `{"Name": "Latte", "Roast": "Light", "Price": "invalid_number"}`
+
+			request, _ = http.NewRequest(http.MethodPut, "/api/v1/coffees/coffee/12345", bytes.NewBuffer([]byte(invalidJson)))
+			request.Header.Set("Content-Type", "application/json")
+
+			controllers.UpdateCoffeeById(recorder, request, mockedCoffee)
+
+			Expect(recorder.Code).To(Equal(http.StatusInternalServerError))
+			responseBody := recorder.Body.String()
+			Expect(responseBody).To(ContainSubstring("cannot unmarshal string into Go struct field Coffee.price of type float32"))
+
+		})
+		It("Should fail at database error", func() {
+			mockCoffee = services.Coffee{
+				ID:        "",
+				Name:      "Latte",
+				Roast:     "Light",
+				Image:     "image2.png",
+				Region:    "Colombia",
+				Price:     12.0,
+				GrindUnit: 1,
+				CreatedAt: time.Now(),
+				UpdatedAt: time.Now(),
+			}
+			// Marshal the struct to JSON
+			mockCoffeeJson, _ = json.Marshal(mockCoffee)
+			request, _ = http.NewRequest(http.MethodPut, "/api/v1/coffees/coffee/12345", bytes.NewBuffer(mockCoffeeJson))
+			request.Header.Set("Content-Type", "application/json")
+			mockedCoffee.On("UpdateCoffee", "", mock.MatchedBy(func(c services.Coffee) bool {
+				return c.Name == mockCoffee.Name &&
+					c.Roast == mockCoffee.Roast &&
+					c.Image == mockCoffee.Image &&
+					c.Region == mockCoffee.Region &&
+					c.Price == mockCoffee.Price &&
+					c.GrindUnit == mockCoffee.GrindUnit
+			})).Return(nil, errors.New("Database error"))
+
+			controllers.UpdateCoffeeById(recorder, request, mockedCoffee)
+
+			Expect(recorder.Code).To(Equal(http.StatusInternalServerError))
+
+			responseBody := recorder.Body.String()
+			Expect(responseBody).To(ContainSubstring("Database error"))
+
+		})
+	})
+	Describe("DeleteCoffe", func() {
+
+		BeforeEach(func() {
+			request, _ = http.NewRequest(http.MethodDelete, "/api/v1/coffees/coffee/12345", nil)
+		})
+		It("Should succesfull delete coffee", func() {
+			mockedCoffee.On("DeleteCoffee", "").Return(nil)
+			controllers.DeleteCoffee(recorder, request, mockedCoffee)
+			Expect(recorder.Code).To(Equal(http.StatusOK))
+		})
+		It("Should fail on database error", func() {
+			mockedCoffee.On("DeleteCoffee", "").Return(errors.New("Database error"))
+			controllers.DeleteCoffee(recorder, request, mockedCoffee)
+			Expect(recorder.Code).To(Equal(http.StatusInternalServerError))
+
+			responseBody := recorder.Body.String()
+			Expect(responseBody).To(ContainSubstring("Database error"))
+		})
+	})
 })
